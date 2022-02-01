b33d9366bf0c5a453351e7e3724e13bfa0dadc61
hy <zihong2020@foxmail.com>
2022-02-01 23:36:40 +0800

    ci(ci): init project devops

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: hy
+ * @Date: 2022-02-01 23:36:19
+ * @LastEditors: hy
+ * @Description:
+ * @LastEditTime: 2022-02-01 23:36:25
+ * @FilePath: /instaclone-backend/commitlint.config.js
+ * @Copyright 2022 hy, All Rights Reserved.
+ * @仅供学习使用~
+ **/
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
