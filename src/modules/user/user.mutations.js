24c4a57b044fa63e911927767f87f5f94c59b309
hy <zihong2020@foxmail.com>
2022-02-03 23:21:04 +0800

    fix(user): fix user model create

--- src/modules/user/user.mutations.js
+++ src/modules/user/user.mutations.js
@@ -3,7 +3,7 @@
  * @Date: 2022-02-03 15:50:21
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-02-03 21:05:06
+ * @LastEditTime: 2022-02-03 23:17:13
  * @FilePath: /instaclone-backend/src/modules/user/user.mutations.js
  * @Copyright 2022 hy, All Rights Reserved.
  * @仅供学习使用~
@@ -12,43 +12,49 @@ import clint from "../../client";
 import bcrypt from "bcrypt";
 
 export default {
-  createUser: async (_, { firstName, lastName, userName, email, password }) => {
-    console.log("param ==>", firstName, lastName, userName, email, password);
-    try {
-      const isHaveUser = await clint.user.findFirst({
-        where: { OR: [{ userName }, { email }] },
-      });
-      console.log("isHaveUser  ==>", isHaveUser);
-      if (isHaveUser) {
-        throw new Error("This username/password is already taken.");
+  Mutation: {
+    createUser: async (
+      _,
+      { firstName, lastName, userName, email, password }
+    ) => {
+      console.log("param ==>", firstName, lastName, userName, email, password);
+      try {
+        const isHaveUser = await clint.user.findFirst({
+          where: { OR: [{ userName }, { email }] },
+        });
+        console.log("isHaveUser  ==>", isHaveUser);
+        if (isHaveUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        // create : password -> MD5 -（后端）> MD5 -> db
+        // login : password -> MD5 -> MD5 -（后端) -> db
+        const uglyPassword = await bcrypt.hash(password, 10);
+        console.log("uglyPassword ==>", uglyPassword);
+        // let user = await client.user.create({
+        //   data: {
+        //     userName,
+        //     email,
+        //     firstName,
+        //     lastName,
+        //     password: uglyPassword,
+        //   },
+        // });
+        // console.log("createUser ==>", user);
+        // return user;
+        return await clint.user.create({
+          data: {
+            userName,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        // return ture;
+      } catch (error) {
+        console.log(error);
+        return error;
       }
-      // create : password -> MD5 -（后端）> MD5 -> db
-      // login : password -> MD5 -> MD5 -（后端) -> db
-      const uglyPassword = await bcrypt.hash(password, 10);
-      console.log("uglyPassword ==>", uglyPassword);
-      // let user = await client.user.create({
-      //   data: {
-      //     userName,
-      //     email,
-      //     firstName,
-      //     lastName,
-      //     password: uglyPassword,
-      //   },
-      // });
-      // console.log("createUser ==>", user);
-      // return user;
-      return await clint.user.create({
-        data: {
-          userName,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
-      // return ture;
-    } catch (error) {
-      return error;
-    }
+    },
   },
 };
--- src/schema.js
+++ src/schema.js
@@ -3,13 +3,15 @@
  * @Date: 2022-02-02 12:37:32
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-02-03 15:17:22
+ * @LastEditTime: 2022-02-03 23:12:33
  * @FilePath: /instaclone-backend/src/schema.js
  * @Copyright 2022 hy, All Rights Reserved.
  * @仅供学习使用~
  **/
 import { loadFilesSync, mergeTypeDefs, mergeResolvers } from "graphql-tools";
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedMutation = loadFilesSync(`${__dirname}/**/*.{mutation,queries}.js`);
+const loadedMutation = loadFilesSync(
+  `${__dirname}/**/*.{mutations,queries}.js`
+);
 export const typeDefs = mergeTypeDefs(loadedTypes);
 export const resolvers = mergeResolvers(loadedMutation);
