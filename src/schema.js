db43f327df2d2a415df7121837cb9e6f1e8b6f34
hy <zihong2020@foxmail.com>
2022-02-03 15:18:37 +0800

    feat(db): delete db module

--- src/schema.js
+++ src/schema.js
@@ -3,25 +3,13 @@
  * @Date: 2022-02-02 12:37:32
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-02-03 14:47:23
+ * @LastEditTime: 2022-02-03 15:17:22
  * @FilePath: /instaclone-backend/src/schema.js
  * @Copyright 2022 hy, All Rights Reserved.
  * @仅供学习使用~
  **/
-import {
-  // makeExecutableSchema,
-  loadFilesSync,
-  mergeTypeDefs,
-  mergeResolvers,
-} from "graphql-tools";
-// import {
-//   loadFilesSync,
-//   mergeTypeDefs,
-//   mergeResolvers,
-// } from "@graphql-tools/schema";
-// const typesArray = loadFilesSync(path.join(__dirname, "."), { recurive: true });
+import { loadFilesSync, mergeTypeDefs, mergeResolvers } from "graphql-tools";
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedMutation = loadFilesSync(`${__dirname}/**/*.{mutation,queries}.js`);
 export const typeDefs = mergeTypeDefs(loadedTypes);
 export const resolvers = mergeResolvers(loadedMutation);
-// export const schema = makeExecutableSchema({ typeDefs, resolvers });
--- src/server.js
+++ src/server.js
@@ -3,18 +3,15 @@
  * @Date: 2022-02-01 22:49:24
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-02-03 14:44:07
+ * @LastEditTime: 2022-02-03 15:17:15
  * @FilePath: /instaclone-backend/src/server.js
  * @Copyright 2022 hy, All Rights Reserved.
  * @仅供学习使用~
  **/
 import { typeDefs, resolvers } from "./schema";
-// import { makeExecutableSchema } from "@graphql-tools/schema";
 import { makeExecutableSchema } from "graphql-tools";
 require("dotenv").config(); // env
 import { ApolloServer } from "apollo-server";
-// import { schema } from "./schema";
-// const server = new ApolloServer({ schema });
 const schema = makeExecutableSchema({
   typeDefs: typeDefs,
   resolvers: resolvers,
